a4b166c42197c9896038d35309bd5f8b6b1abd04
Duc Tran <tv.duc95@gmail.com>
2020-07-12 21:04:43 +0700

    Add source projects

---
+++ src/Common/BookReader.GenericDapperRepository/GenericDapperRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookReader.GenericDapperRepository
+{
+    public class GenericDapperRepository: IGenericDapperRepository
+    {
+    }
+}
---
+++ src/Common/BookReader.GenericEFRepository/GenericEFRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookReader.GenericEFRepository
+{
+    public class GenericEFRepository: IGenericEFRepository
+    {
+    }
+}
